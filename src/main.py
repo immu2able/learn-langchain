ef2f9e9b8a3c99f70731f06063086ba40942805d
Kumar <muthukumar.koushik@gmail.com>
2024-02-25 11:39:54 +0000

    web based example

--- src/main.py
+++ src/main.py
@@ -1,5 +1,54 @@
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.documents import Document
+from langchain.chains import create_retrieval_chain
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.llms import Ollama
+
 llm = Ollama(model="llama2")
 
+embeddings = OllamaEmbeddings()
 
-    
+loader = WebBaseLoader("https://docs.smith.langchain.com")
+
+docs = loader.load()
+
+
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = FAISS.from_documents(documents, embeddings)
+
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "You are world class technical documentation writer."),
+#     ("user", "{input}")
+# ])
+
+
+prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {input}""")
+
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+
+
+document_chain.invoke({
+    "input": "how can langsmith help with testing?",
+    "context": [Document(page_content="langsmith can let you visualize test results")]
+})
+
+
+retriever = vector.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+response = retrieval_chain.invoke({"input": "how can langsmith help with testing?"})
+print(response["answer"])
+
+# LangSmith offers several features that can help with testing:...
